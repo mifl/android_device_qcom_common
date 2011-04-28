45de179a9239be6f61c7b2c719bbdd93a1916e37
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-04-28 20:18:48 +0530

    Add Modules to PRODUCT_PACKAGES list
    Modules which do not have LOCAL_MODULE_TAGS defined default
    to "user" tag. This results in errors and warning messages
    in the Android build system in Gingerbread. These errors and
    warning messages can be eliminated by adding "optional" tag.
    When we add "optional" tag to any of the modules, the module
    should be part of PRODUCT_PACKAGES list, to be available in
    the final images that we want to load on to the target.
    This change adds modules to PRODUCT_PACKAGES list.
    
    Change-Id: Ib3c68725e91e2e6c4ac74dd706c0c67ce4cdc684

--- common.mk
+++ common.mk
@@ -1,3 +1,96 @@
+# Below projects/packages with LOCAL_MODULEs will be used by
+# PRODUCT_PACKAGES to build LOCAL_MODULEs that are tagged with
+# optional tag, which will not be available on target unless
+# explicitly list here. Where project corresponds to the vars here
+# in CAPs.
+
+#KERNEL_TESTS
+KERNEL_TESTS := mm-audio-native-test
+
+#KEYPAD
+KEYPAD := ffa-keypad_qwerty.kcm
+KEYPAD += ffa-keypad_numeric.kcm
+KEYPAD += fluid-keypad_qwerty.kcm
+KEYPAD += fluid-keypad_numeric.kcm
+KEYPAD += surf_keypad_qwerty.kcm
+KEYPAD += surf_keypad_numeric.kcm
+KEYPAD += surf_keypad.kcm
+KEYPAD += 7k_ffa_keypad.kcm
+
+#LIBCAMERA
+LIBCAMERA := libcamera
+
+#LIBCOPYBIT
+LIBCOPYBIT := copybit.msm8660
+LIBCOPYBIT += copybit.msm7k
+LIBCOPYBIT += copybit.qsd8k
+
+#LIBGRALLOC
+LIBGRALLOC := gralloc.default
+LIBGRALLOC += gralloc.msm8660
+LIBGRALLOC += gralloc.msm7k
+
+#LIBLIGHTS
+LIBLIGHTS := lights.msm8660
+LIBLIGHTS += lights.msm7k
+
+#LIBOPENCOREHW
+LIBOPENCOREHW := libopencorehw
+
+#LIBOVERLAY
+LIBOVERLAY := liboverlay
+LIBOVERLAY += overlay.default
+
+#LOC_API
+LOC_API := libloc_api-rpc-qc
+
+#MM_AUDIO
+MM_AUDIO := libOmxAacDec
+MM_AUDIO += libOmxAacEnc
+MM_AUDIO += libOmxAmrEnc
+MM_AUDIO += libOmxEvrcEnc
+MM_AUDIO += libOmxMp3Dec
+MM_AUDIO += libOmxQcelp13Enc
+MM_AUDIO += mm-adec-omxaac-test
+MM_AUDIO += mm-adec-omxmp3-test
+MM_AUDIO += mm-aenc-omxaac-test
+MM_AUDIO += mm-aenc-omxamr-test
+MM_AUDIO += mm-aenc-omxevrc-test
+MM_AUDIO += mm-aenc-omxqcelp13-test
+MM_AUDIO += sw-adec-omxaac-test
+MM_AUDIO += sw-adec-omxamr-test
+MM_AUDIO += sw-adec-omxamrwb-test
+MM_AUDIO += sw-adec-omxmp3-test
+
+#MM_CORE
+MM_CORE := libmm-omxcore
+MM_CORE += libOmxCore
+
+#MM_VIDEO
+MM_VIDEO := ast-mm-vdec-omx-test
+MM_VIDEO += libdivxdrmdecrypt
+MM_VIDEO += liblasic
+MM_VIDEO += libOmxVdec
+MM_VIDEO += libOmxVenc
+MM_VIDEO += libOmxVidEnc
+MM_VIDEO += mm-vdec-omx-property-mgr
+MM_VIDEO += mm-vdec-omx-test
+MM_VIDEO += mm-venc-omx-test
+MM_VIDEO += mm-venc-omx-test720p
+MM_VIDEO += mm-video-driver-test
+MM_VIDEO += mm-video-encdrv-test
+
+#PVOMX
+PVOMX := libqcomm_omx
+
+#SOFTAP
+SOFTAP := libQWiFiSoftApCfg
+SOFTAP += libqsap_sdk
+
+#UPDATER
+UPDATER := librecovery_ui_qcom
+UPDATER += librecovery_updater_qcom
+
 PRODUCT_PACKAGES := \
     AccountAndSyncSettings \
     DeskClock \
@@ -26,6 +119,22 @@ PRODUCT_PACKAGES := \
     IM \
     VoiceDialer \
     FM
+
+PRODUCT_PACKAGES += $(KERNEL_TESTS)
+PRODUCT_PACKAGES += $(KEYPAD)
+PRODUCT_PACKAGES += $(LIBCAMERA)
+PRODUCT_PACKAGES += $(LIBCOPYBIT)
+PRODUCT_PACKAGES += $(LIBGRALLOC)
+PRODUCT_PACKAGES += $(LIBLIGHTS)
+PRODUCT_PACKAGES += $(LIBOPENCOREHW)
+PRODUCT_PACKAGES += $(LIBOVERLAY)
+PRODUCT_PACKAGES += $(LOC_API)
+PRODUCT_PACKAGES += $(MM_AUDIO)
+PRODUCT_PACKAGES += $(MM_CORE)
+PRODUCT_PACKAGES += $(MM_VIDEO)
+PRODUCT_PACKAGES += $(PVOMX)
+PRODUCT_PACKAGES += $(SOFTAP)
+PRODUCT_PACKAGES += $(UPDATER)
 
 PRODUCT_COPY_FILES := \
     frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
