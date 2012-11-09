11e857ff65c179d2c66f35abb9edeee573b6c4f0
Ramakant Singh <ramaka@codeaurora.org>
2012-11-09 11:57:33 +0530

    qcom/common: set the DPI at boot time for non-overlay  tagets
    Change-Id: I3105387a1695205ab48f446d8235c070817865bb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,6 +11,14 @@ LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.unicorn-dpi.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.unicorn-dpi.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
