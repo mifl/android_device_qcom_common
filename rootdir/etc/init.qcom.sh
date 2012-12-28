7563d8a4746383a44958ea10b0fcd0e66504049e
Dhivya Subramanian <dthiru@codeaurora.org>
2012-12-28 13:28:42 -0800

    Revert "Include check for MSM8930 in init script"
    This reverts commit 86efcacb8cd7010de3a05a7427a49a9e0e61a35a.

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -27,7 +27,7 @@
 #
 
 target=`getprop ro.board.platform`
-platformid=`cat /sys/devices/system/soc/soc0/id`
+
 #
 # Function to start sensors for DSPS enabled platforms
 #
@@ -120,9 +120,7 @@ case "$target" in
         esac
         ;;
     "msm8960")
-        if[ "$platformid" != "116" && "$platformid" != "142"]; then
-            start_sensors
-        fi
+        start_sensors
         case "$baseband" in
             "msm")
 		start_battery_monitor;;
