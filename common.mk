c84bb55e13b7a038df8077e5f0852716e10897f0
Michael Vines <mvines@codeaurora.org>
2013-05-25 20:44:32 -0700

    Remove LivePicker .xml file
    Change-Id: I02568a19cdb9f7ab1b1a61fcfc56b2adf573f5b6

--- common.mk
+++ common.mk
@@ -612,7 +612,6 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
 
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
