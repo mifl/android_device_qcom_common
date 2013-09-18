4124d199dbc19d4c03c7f62b131b6a6bb2ea9b80
Juffin Alex Varghese <jalex@codeaurora.org>
2013-09-18 16:57:50 +0530

    Common: Disable Bluez components in MR2
    Since MR2 comes with Bluedroid stack, disable Bluez dependant
    components in MR2.
    
    CRs-Fixed: 542077
    Change-Id: I0a903741cf3fce8c42cfc29a5d8adaf6e3ed8d32

--- common.mk
+++ common.mk
@@ -81,7 +81,7 @@ BSON := libbson
 #BT
 BT := javax.btobex
 BT += libattrib_static
-BT += hcidump.sh
+
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 
 #C2DColorConvert
@@ -560,19 +560,6 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 #    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
-
-# Bluetooth configuration files
-#PRODUCT_COPY_FILES += \
-    system/bluetooth/data/audio.conf:system/etc/bluetooth/audio.conf \
-    system/bluetooth/data/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
-    system/bluetooth/data/blacklist.conf:system/etc/bluetooth/blacklist.conf \
-    system/bluetooth/data/input.conf:system/etc/bluetooth/input.conf \
-    system/bluetooth/data/network.conf:system/etc/bluetooth/network.conf \
-
-ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ),true)
-#PRODUCT_COPY_FILES += \
-    system/bluetooth/data/stack.conf:system/etc/bluetooth/stack.conf
-endif # BOARD_HAVE_BLUETOOTH_BLUEZ
 
 PRODUCT_COPY_FILES += device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml \
                       device/qcom/common/media/media_codecs.xml:system/etc/media_codecs.xml
