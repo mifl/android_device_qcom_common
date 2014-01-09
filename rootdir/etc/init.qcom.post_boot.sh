2f84e873ddf6aef2dde09ba0f0705de52d275fe3
Darren <darren.mohle@dts.com>
2014-01-09 12:56:39 -0800

    Revert "DTS Eagle integration (device)"
    This reverts commit 27ed9b3523aecbac488f65cb9282ca65d373a57e.

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -245,7 +245,6 @@ esac
 
 case "$target" in
     "msm8974")
-        /system/bin/dts_configurator
         echo 2 > /sys/module/lpm_resources/enable_low_power/l2
         echo 1 > /sys/module/lpm_resources/enable_low_power/pxo
         echo 1 > /sys/module/lpm_resources/enable_low_power/vdd_dig
