614dc071055b11f5aa43f774927a0b6ff0aa428f
Indrajeet Kumar <ikumar@codeaurora.org>
2014-03-03 17:47:41 -0800

    init.qcom.usb.sh: Allow "none" value of persist.sys.usb.config
    Change-Id: Id9a02ab31b795dd56c1256dea31e590b50902e26

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -87,7 +87,7 @@ baseband=`getprop ro.baseband`
 echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
 usb_config=`getprop persist.sys.usb.config`
 case "$usb_config" in
-    "" | "adb") #USB persist config not set, select default configuration
+    "" | "none" | "adb") #USB persist config not set, select default configuration
         case $target in
             "msm8960" | "msm8974" | "msm8226" | "msm8610" | "apq8084")
                 case "$baseband" in
