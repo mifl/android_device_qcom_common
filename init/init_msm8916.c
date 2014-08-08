967c29dfeeff436e38f13b928ab1a309bd84923b
Srinu Gorle <sgorle@codeaurora.org>
2014-08-08 18:21:11 +0530

    init: Add soc_id to enabled hevc video decoder for 8936
    Enable set property media.hwhevccodectype, if soc_id is identified as 233.
    
    CRs-Fixed: 706166
    Change-Id: I80e09cb8274c2e9789a01cd125f202902d0bc6e1

--- init/init_msm8916.c
+++ init/init_msm8916.c
@@ -63,7 +63,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     else
         property_set(PROP_LCDDENSITY, "320");
 
-    if (msm_id >= 239 && msm_id <= 243) {
+    if ((msm_id >= 239 && msm_id <= 243) || msm_id == 233) {
          property_set("media.hwhevccodectype", "1");
     } else if (msm_id == 206) {
         property_set("media.swhevccodectype", "1");
