eba53bb0a11800b4ed9d1a1557306baec445c2c3
Pracheer <pracheer@codeaurora.org>
2014-08-12 00:32:23 +0530

    msm8936: post_boot.sh: Interactive settings for 8936
    Apply interactive governor settings for 8936.
    
    Change-Id: I8eef7d9c4718c1fc6fe38ce0f72401d8315eec3a

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -555,6 +555,22 @@ case "$target" in
             ;;
         esac
 
+	# Apply governor settings for 8936
+        case "$soc_id" in
+            "233")
+                echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                echo "25000 1113600:50000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                echo 25000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+                echo 960000 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+                echo "85 1113600:90 1267200:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
+                echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+            ;;
+        esac
+
         # Apply governor settings for 8939
         case "$soc_id" in
             "239")
