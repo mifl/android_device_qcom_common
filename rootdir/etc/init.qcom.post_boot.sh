ed565a040d9b4cb77a915e313f6f231ff2523fb5
Vince Leung <vincentl@codeaurora.org>
2014-08-11 23:09:30 -0700

    init.qcom.post_boot: enable perfd for msm8994
    msm8994 will use perfd instead of mpdecision
    to host PerfLock
    
    Change-Id: Ib95952fdd6b8eea472cdb50694e959658eff9a42

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -718,6 +718,9 @@ case "$target" in
         start mpdecision
         echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
     ;;
+    "msm8994")
+        start perfd
+    ;;
     "apq8084")
         rm /data/system/default_values
         start mpdecision
