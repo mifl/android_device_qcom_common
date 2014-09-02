5f875795a3d89d4a0a85f8bb057075cd86f9ee12
Sreedhar Lakkaraju <slakkara@codeaurora.org>
2014-09-02 22:05:49 +0530

    common: add support to sign boot image
    adding rules for generating signed boot image
    
    Change-Id: I53710c2b4b8e8750c29d9c46c51a3e4ff0f270d2

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -387,6 +387,14 @@ endif
 
 endif
 
+#----------------------------------------------------------------------
+# Generate secure boot image
+#----------------------------------------------------------------------
+ifeq ($(TARGET_BOOTIMG_SIGNED),true)
+.PHONY: bootimage
+bootimage: $(INSTALLED_BOOTIMAGE_TARGET) $(INSTALLED_SEC_BOOTIMAGE_TARGET)
+endif
+
 ###################################################################################################
 
 .PHONY: aboot
