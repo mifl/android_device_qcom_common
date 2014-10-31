a2d8a5ebaaa7c48514b9b7df042e5b8294e3c747
Gopal G Goberu <ggober@codeaurora.org>
2014-11-01 03:48:11 +0530

    compilation fix
    Change-Id: I1d2cd40766fa0b3585223591edb0819075ec905a

--- base.mk
+++ base.mk
@@ -757,10 +757,10 @@ PRODUCT_COPY_FILES := \
 #endif # BOARD_HAVE_BLUETOOTH_BLUEZ
 
 # gps/location secuity configuration file
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     device/qcom/common/sec_config:system/etc/sec_config
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
@@ -783,4 +783,4 @@ PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
 
 # dm-verity definitions
 PRODUCT_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
+$(call inherit-product-if-exists, build/target/product/verity.mk)
