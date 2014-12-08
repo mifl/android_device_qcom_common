3e474d745113d1dba1c972e268452611399522b8
AUDITYA BHATTARAM <audityab@codeaurora.org>
2014-12-08 14:58:55 +0530

    Build Secure boot.img & recovery.img for ota.
    Build Secure boot.img & recovery.img for otapackage.
    
    Change-Id: I7b859eef64306357937169f825c90c5f7cd07968

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -389,6 +389,11 @@ endif
 
 ###################################################################################################
 
+ifeq ($(TARGET_BOOTIMG_SIGNED),true)
+.PHONY: otapackage
+otapackage: $(INSTALLED_SEC_BOOTIMAGE_TARGET) $(INSTALLED_SEC_RECOVERYIMAGE_TARGET)
+endif
+
 .PHONY: aboot
 aboot: $(INSTALLED_BOOTLOADER_MODULE)
 
