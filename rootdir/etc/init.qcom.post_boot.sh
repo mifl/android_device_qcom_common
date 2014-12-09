87f65356e66bdd0c8b87dc6759e7f816494eedae
Murali Nalajala <mnalajal@codeaurora.org>
2014-12-08 18:23:42 -0800

    init.qcom.post_boot: Enable LPM modes for 8992
    Enable LPM modes for 8992 and bring A57 cores online
    
    Change-Id: Ifb22b84ea45d8b79f5a2f54391d69ffc68bf3375

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -734,6 +734,15 @@ case "$target" in
 esac
 
 case "$target" in
+    "msm8992")
+        echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+        # online A57 cores
+        echo 1 > /sys/devices/system/cpu/cpu4/online
+        echo 1 > /sys/devices/system/cpu/cpu5/online
+    ;;
+esac
+
+case "$target" in
     "msm8994")
         echo 0 > /sys/module/msm_thermal/core_control/enabled
         echo -n disable > /sys/devices/soc.*/qcom,bcl.*/mode
