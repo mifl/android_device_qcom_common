fa576fa6004f263d9c2b20fc9865040bf4cd0285
Sandeep Gutta <sangutta@codeaurora.org>
2014-12-23 16:48:46 +0530

    Telephony: set default subscription mode to RUIM
    Set default subscription mode(ro.telephony.default_cdma_sub)
    to RUIM.
    
    Change-Id: Ic7fb3843849bb5a6ad078bb6d451b6be6333ed66
    CRs-Fixed: 773637

--- common.mk
+++ common.mk
@@ -738,7 +738,8 @@ PRODUCT_LOCALES += hdpi mdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqc-opt.so \
-    persist.radio.apm_sim_not_pwdn=1
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.telephony.default_cdma_sub=0
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 
