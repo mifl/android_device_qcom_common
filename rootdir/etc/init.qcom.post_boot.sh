de2347fb9e424f17f46f981c28e3742891f79e79
Vince Leung <vincentl@codeaurora.org>
2015-02-27 13:39:06 -0800

    init: qcom: post_boot: set mincpubw devfreq governor to cpufreq for 8994
    Enable CPU to DDR bus scaling flooring by setting mincpubw
    devfreq governor to cpufreq.
    
    Change-Id: I468deaa1d726ae7c7ef5e677e72c4f5793fcca37

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -839,6 +839,10 @@ case "$target" in
         do
             echo "bw_hwmon" > $devfreq_gov
         done
+        for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
+        do
+            echo "cpufreq" > $devfreq_gov
+        done
     ;;
 esac
 
