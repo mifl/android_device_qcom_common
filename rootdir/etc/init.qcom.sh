20de84a0651f29965c9bdc68a859939eddf90ea8
Sooraj Sasindran <ssasin@codeaurora.org>
2015-03-20 18:37:02 -0600

    Change bootup shell script to duplicate config files
    Change bootup shell script to:
    - Copy config files
    - change the new config to correct ownship.
    - set flag to indicate copy is completed.
    
    Change-Id: Ie00059ba49ac0d59b2c41b35640593347521dec3

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -94,6 +94,17 @@ start_vm_bms()
 	fi
 }
 
+
+#
+# Make modem config folder and copy firmware config to that folder
+#
+rm -rf /data/misc/radio/modem_config
+mkdir /data/misc/radio/modem_config
+chmod 770 /data/misc/radio/modem_config
+cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
+chown -hR radio.radio /data/misc/radio/modem_config
+echo 1 > /data/misc/radio/copy_complete
+
 baseband=`getprop ro.baseband`
 #
 # Suppress default route installation during RA for IPV6; user space will take
