dd27d8d94328923892b02b08d9a164b58a0da143
Srinu Gorle <sgorle@codeaurora.org>
2015-03-23 13:14:42 +0530

    common: Add new soc id in msm8929 list for hw HEVC decoder
    Add new soc id in 8929 target verification to enabled hw HEVC decoder.
    
    Change-Id: I6a5947c92caca3908b1ad6bae43e202d600783c7

--- init/init_msm8916.c
+++ init/init_msm8916.c
@@ -69,7 +69,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     } else
         property_set(PROP_LCDDENSITY, "320");
 
-    if ((msm_id >= 239 && msm_id <= 243) || msm_id == 233) {
+    if ((msm_id >= 239 && msm_id <= 243) || (msm_id >= 268 && msm_id <= 271) ||
+         msm_id == 233 || msm_id == 282) {
          property_set("media.hwhevccodectype", "1");
     } else if (msm_id == 206) {
         property_set("media.swhevccodectype", "1");
