d5e472633e5c7619b1367913aef30bd443cfb7d8
Srinivas Rao L <lsrao@codeaurora.org>
2015-03-24 17:00:12 +0530

    init: qcom: post_boot: disable l2-gdhs lpm for 8939-V3.0
    Disable l2-gdhs mode for 8939-V3.0
    
    Change-Id: I26de005d3fac2f56f305ce97679a20a25463211d

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -349,9 +349,50 @@ case "$target" in
 		echo 1 > /sys/devices/system/cpu/cpu2/online
 	        echo 1 > /sys/devices/system/cpu/cpu3/online
 	    ;;
-           "239" | "241" | "263" | "268" | "269" | "270" | "271")
-		echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-		echo 10 > /sys/class/net/rmnet0/queues/rx-0/rps_cpus
+            "239" | "241" | "263")
+               if [ -f /sys/devices/soc0/revision ]; then
+                   revision=`cat /sys/devices/soc0/revision`
+               else
+                   revision=`cat /sys/devices/system/soc/soc0/revision`
+               fi
+               case "$revision" in
+                   "3.0")
+                       echo N > /sys/module/lpm_levels/system/power/power-l2-gdhs/idle_enabled
+                       echo N > /sys/module/lpm_levels/system/performance/performance-l2-gdhs/idle_enabled
+                   ;;
+               esac
+               echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+               echo 10 > /sys/class/net/rmnet0/queues/rx-0/rps_cpus
+                if [ -f /sys/devices/soc0/platform_subtype_id ]; then
+                    platform_subtype_id=`cat /sys/devices/soc0/platform_subtype_id`
+                fi
+                if [ -f /sys/devices/soc0/hw_platform ]; then
+                    hw_platform=`cat /sys/devices/soc0/hw_platform`
+                fi
+                case "$soc_id" in
+                    "239")
+                    case "$hw_platform" in
+                        "Surf")
+                            case "$platform_subtype_id" in
+                                "1" | "2")
+                                    start hbtp
+                                ;;
+                            esac
+                        ;;
+                        "MTP")
+                            case "$platform_subtype_id" in
+                                "3")
+                                    start hbtp
+                                ;;
+                            esac
+                        ;;
+                    esac
+                    ;;
+                esac
+            ;;
+            "268" | "269" | "270" | "271")
+                echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+                echo 10 > /sys/class/net/rmnet0/queues/rx-0/rps_cpus
             ;;
              "233" | "240" | "242")
 		echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
