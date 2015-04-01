3010fed3b9f6929f4968967af7e7f839e530d42f
Preeti Ahuja <preetia@codeaurora.org>
2015-03-31 17:07:20 -0700

    Enable Embms time sync feature by default.
    The Embms time sync feature is controlled by persist.radio.sib16_support
    property. Change the default value of this property to 1 and therefore
    enable the time sync feature by default.
    
    Change-Id: I06a4fe04f74be21162e5eab992ce178a0fcb7a35
    CRs-Fixed: 799348

--- common.mk
+++ common.mk
@@ -16,7 +16,8 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqc-opt.so \
-    persist.radio.apm_sim_not_pwdn=1
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.sib16_support=1
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 
