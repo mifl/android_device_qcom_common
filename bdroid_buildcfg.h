540ea21764ea313692ac703aa32b55d39de838b5
Uday Kishore Pasupuleti <upasupul@codeaurora.org>
2015-04-29 10:27:30 -0700

    Include common Clockwork bluedroid definitions
    update BT configuration to match Google guidelines.
    
    Change-Id: I67615c8df9abb6910d3670539d9ae37c274da6aa

--- bdroid_buildcfg.h
+++ bdroid_buildcfg.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *  Not a Contribution, Apache license notifications and license are retained
  *  for attribution purposes only.
  *
@@ -21,9 +21,19 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
+/* Include common Clockwork bluedroid definitions */
+#include <bdroid_buildcfg_common.h>
+
+
 #define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define BLE_PERIPHERAL_MODE_SUPPORT   TRUE
+#define BLE_PERIPHERAL_DISPLAYONLY    TRUE
+#define BLE_DELAY_REQUEST_ENC         TRUE
+#define BLE_LOCAL_PRIVACY_ENABLED     FALSE
+
 #define MAX_L2CAP_CHANNELS    14
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
@@ -37,11 +47,14 @@
 #define AVRC_INCLUDED                 TRUE
 #define PAN_NAP_DISABLED              TRUE
 
-#define BTA_FTS_OPS_IDLE_TO_SNIFF_DELAY_MS 1000
-
 #define BTA_DM_PM_SNIFF_MAX      2048
 #define BTA_DM_PM_SNIFF_MIN      1024
 #define BTA_DM_PM_SNIFF_ATTEMPT  4
 #define BTA_DM_PM_SNIFF_TIMEOUT  1
+
+#define GAP_TRANSPORT_SUPPORTED       GATT_TRANSPORT_LE
+#define GATTP_TRANSPORT_SUPPORTED     GATT_TRANSPORT_LE
+
+#define BTM_DEFAULT_SCAN_TYPE         BTM_SCAN_TYPE_STANDARD
 
 #endif
