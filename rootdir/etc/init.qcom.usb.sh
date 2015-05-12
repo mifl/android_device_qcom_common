cc84a013a6aa379b3f03fb15b90d6efefa1472df
Yu Guoping <guopingy@codeaurora.org>
2015-05-12 16:49:25 +0800

    USB: Add RPS CPU mask for rndis interface for 8929
    Add the RPS CPU mask as 10 on 8929 target. Target
    identification is done using soc ids for 8929
    target.
    
    Change-Id: I895e78f56e60cabda1ba4581d0fd25ac3021079f

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -240,10 +240,10 @@ else
 	soc_id=`cat /sys/devices/system/soc/soc0/id`
 fi
 
-# enable rps cpus on msm8939 target
+# enable rps cpus on msm8939/msm8929 target
 setprop sys.usb.rps_mask 0
 case "$soc_id" in
-	"239" | "241" | "263")
+	"239" | "241" | "263" | "268" | "269" | "270")
 		setprop sys.usb.rps_mask 10
 	;;
 esac
