feb0512eccc2bff4cbbaac7e7e95f6a21f0342ab
Hua Liu <richardl@codeaurora.org>
2015-05-13 19:48:37 +0800

    Add execute permission for mbn directory
    Add execute permssion for /data/misc/radio/modem_config/ so
    that radio apps could access it
    
    Change-Id: Ie57c20a5ee234c555613e454f65532847f7bcfe0

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -277,7 +277,7 @@ echo 1 > /data/misc/radio/db_check_done
 #
 rm -rf /data/misc/radio/modem_config
 mkdir /data/misc/radio/modem_config
-chmod 660 /data/misc/radio/modem_config
+chmod 770 /data/misc/radio/modem_config
 cp -r /firmware/image/modem_pr/mbn_ota/* /data/misc/radio/modem_config
 chown -hR radio.radio /data/misc/radio/modem_config
 echo 1 > /data/misc/radio/copy_complete
