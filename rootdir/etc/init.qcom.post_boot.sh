9a2d1c376cb9e67c6b711e955789277afee4d21a
Karthik Parsha <kparsha@codeaurora.org>
2015-06-02 11:34:29 -0700

    APQ8064: init: Remove mpdecision for Automotive targets
    Modify init.qcom.post_init.sh to remove mpdecision for
    8960/8064 automotive targets.
    
    Change-Id: I2cd658a42c6efda144be6464368a32b1201e746d

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -182,27 +182,11 @@ case "$target" in
          echo 20000 > /sys/module/msm_dcvs/cores/gpu0/slack_time_max_us
          echo 20000 > /sys/module/msm_dcvs/cores/gpu0/slack_time_min_us
          echo 0 > /sys/module/msm_dcvs/cores/gpu0/slack_mode_dynamic
-         # set msm_mpdecision parameters
-         echo 45000 > /sys/module/msm_mpdecision/slack_time_max_us
-         echo 15000 > /sys/module/msm_mpdecision/slack_time_min_us
-         echo 100000 > /sys/module/msm_mpdecision/em_win_size_min_us
-         echo 1000000 > /sys/module/msm_mpdecision/em_win_size_max_us
-         echo 3 > /sys/module/msm_mpdecision/online_util_pct_min
-         echo 25 > /sys/module/msm_mpdecision/online_util_pct_max
-         echo 97 > /sys/module/msm_mpdecision/em_max_util_pct
-         echo 2 > /sys/module/msm_mpdecision/rq_avg_poll_ms
-         echo 10 > /sys/module/msm_mpdecision/mp_em_rounding_point_min
-         echo 85 > /sys/module/msm_mpdecision/mp_em_rounding_point_max
-         echo 50 > /sys/module/msm_mpdecision/iowait_threshold_pct
          #set permissions for the nodes needed by display on/off hook
          chown -h system /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chown -h system /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chown -h system /sys/module/msm_mpdecision/slack_time_max_us
-         chown -h system /sys/module/msm_mpdecision/slack_time_min_us
          chmod -h 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chmod -h 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chmod -h 664 /sys/module/msm_mpdecision/slack_time_max_us
-         chmod -h 664 /sys/module/msm_mpdecision/slack_time_min_us
          soc_id=`cat /sys/devices/system/soc/soc0/id`
          case "$soc_id" in
              "130")
@@ -344,7 +328,7 @@ case "$target" in
     ;;
 esac
 case "$target" in
-    "msm8660" | "msm8960" | "msm8974")
+    "msm8660" | "msm8974")
         start mpdecision
     ;;
     "msm7627a")
