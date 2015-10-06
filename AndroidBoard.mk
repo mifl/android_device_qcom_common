9dfa3bdcff965b1380a1ce8dacf72f0056540df8
Sivasangeetha.k <sivasangeetha.k@borqs.com>
2015-10-06 15:55:00 +0530

    msm8976: Adding defconfig for POLARIS
    Created new defconfig for POLARIS from msm_defconfig
    
    M0043739 : Polaris Bring-up
    
    Change-Id: I33bbed7814ef8901943230f11b2aa0b74470c7a2
    Signed-off-by: Sivasangeetha.k <sivasangeetha.k@borqs.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,1 +1,12 @@
 include device/qcom/msm8952_64/AndroidBoard.mk
+#----------------------------------------------------------------------
+
+# Compile Linux Kernel
+
+#----------------------------------------------------------------------
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+   KERNEL_DEFCONFIG := polaris-perf_defconfig
+else
+   KERNEL_DEFCONFIG := polaris_defconfig
+endif
