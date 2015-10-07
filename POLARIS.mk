29a4ab7ed328980e8cf563d862a1c23a113306c6
arjun <arjun@borqs.com>
2015-10-07 17:13:19 +0530

    M0043739 : [Polaris] Audio Mic bringup.
    Audio mic configuration for Polaris Bringup
    
    Change-Id: I433185f19934c0d35aa4382e4c4e6254e84c8f5f
    Signed-off-by: arjun <arjun@borqs.com>

--- POLARIS.mk
+++ POLARIS.mk
@@ -1,5 +1,8 @@
 DEVICE_PACKAGE_OVERLAYS := device/qcom/POLARIS/overlay
 
+PRODUCT_COPY_FILES += \
+   device/qcom/POLARIS/mixer_paths_wcd9326.xml:system/etc/mixer_paths_wcd9326.xml
+
 include device/qcom/msm8952_64/msm8952_64.mk
 
 PRODUCT_NAME := POLARIS
