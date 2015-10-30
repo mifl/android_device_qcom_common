dd63ae25322cfe02acccf94af859f70a94d59eb6
manish <manish.chaturvedi@borqs.com>
2015-10-30 12:46:10 +0530

    M0044479 : Device model changed to POLARIS
    Signed-off-by: manish <manish.chaturvedi@borqs.com>
    
    Change-Id: I845477cee19de673ec730519c4eb374ae52cb619

--- POLARIS.mk
+++ POLARIS.mk
@@ -3,8 +3,11 @@ DEVICE_PACKAGE_OVERLAYS := device/qcom/POLARIS/overlay
 PRODUCT_COPY_FILES += \
    device/qcom/POLARIS/mixer_paths_wcd9326.xml:system/etc/mixer_paths_wcd9326.xml
 
+
 include device/qcom/msm8952_64/msm8952_64.mk
+
 
 PRODUCT_NAME := POLARIS
 PRODUCT_DEVICE := POLARIS
+PRODUCT_MODEL := POLARIS
 PRODUCT_CHARACTERISTICS := PHONE
