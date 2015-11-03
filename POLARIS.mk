7513f68787aaf9e1e58edaadc73b4f73a21d9ff8
arjun <arjun@borqs.com>
2015-11-03 14:54:49 +0530

    M0044183 : update the correct WCNSS file
    Change-Id: Ia99d28c4826a9a53101d992ab3172b897e913c8c
    Signed-off-by: arjun <arjun@borqs.com>

--- POLARIS.mk
+++ POLARIS.mk
@@ -1,6 +1,9 @@
 DEVICE_PACKAGE_OVERLAYS := device/qcom/POLARIS/overlay
 
 PRODUCT_COPY_FILES += \
+   device/qcom/POLARIS/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
+
+PRODUCT_COPY_FILES += \
    device/qcom/POLARIS/mixer_paths_wcd9326.xml:system/etc/mixer_paths_wcd9326.xml
 
 
