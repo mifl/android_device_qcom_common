fa7415cc40b7601a81b6b113754f7d7e6b28941e
Ramakant Singh <ramaka@codeaurora.org>
2015-11-13 11:56:36 +0530

    init: early_boot: Set LCD density according to soc id for 8976 variants
    Set ro.sf.lcd_density property to set the screen DPI value for 8976
    variants
    
    Change-Id: I96e0958d2bd4ab537e2b9395dfc0eb3f63177c25

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -172,7 +172,7 @@ case "$target" in
         ;;
       "msm8952")
         case "$soc_hwid" in
-                264)
+                264|277)
                     setprop ro.sf.lcd_density 480
                     ;;
                 278)
@@ -185,6 +185,7 @@ case "$target" in
                     setprop media.settings.xml /etc/media_profiles_8956.xml
                     ;;
                 266)
+                    setprop ro.sf.lcd_density 480
                     setprop media.msm8956hw 1
                     setprop media.settings.xml /etc/media_profiles_8956.xml
                     ;;
