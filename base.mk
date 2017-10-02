833f06405f0e7de24789fb644371324e187a2a4c
Hans Chang <hansc@codeaurora.org>
2017-10-02 15:35:25 -0700

    removed PRODUCT_BOOT_JARS += com.qualcomm.qti.camera not needed by automotive
    Change-Id: I17ae0fc298092dc51f8530d3ac28fcead1bc2e54

--- base.mk
+++ base.mk
@@ -1036,11 +1036,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
     ro.adb.secure=1
 endif
 
-#Camera QC extends API
-ifeq ($(strip $(TARGET_USES_QTIC_EXTENSION)),true)
-PRODUCT_BOOT_JARS += com.qualcomm.qti.camera
-endif
-
 # Preloading QPerformance jar to ensure faster perflocks in Boost Framework
 PRODUCT_BOOT_JARS += QPerformance
 
