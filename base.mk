a69778bf1eac98e97ecafc4d70434d64dfc8faca
Lei Zhang <lzhng@codeaurora.org>
2018-01-18 11:09:05 +0800

    Android:opengles cts issue fix
    Opengles:Add opengles opengl copy for CTS issue
    
    Change-Id: I41b3a23d50abd60eee5a5e196ed1206d75daffb0
    Signed-off-by: Lei Zhang <lzhng@codeaurora.org>

--- base.mk
+++ base.mk
@@ -904,6 +904,7 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
 
 # Bluetooth configuration files
 #PRODUCT_COPY_FILES += \
