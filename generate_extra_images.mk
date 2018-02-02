0cc8f11862fe15de835e20bb61aaab0ad64ce90c
Yogendra Charya Tangalapally <yogendra@codeaurora.org>
2018-02-02 17:16:21 +0530

    Changes to increase cache, decrease system partition sizes for 8905 NAND
    Change-Id: I64979b75d5d80162b5ef7b78401236d3808d87f1

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -435,7 +435,7 @@ define create_system_ubinize_config
   echo vol_type=dynamic >> "$(1)"
   echo vol_name=system >> "$(1)"
   # Reserve 5MB for pushing and syncing with adb
-  echo vol_size=$(shell expr `stat --printf %s $(2)` + 5000000) >> "$(1)"
+  echo vol_size=$(shell expr `stat --printf %s $(2)` + 1048576) >> "$(1)"
 endef
 
 define create_userdata_ubinize_config
@@ -452,7 +452,7 @@ define create_userdata_ubinize_config
   echo vol_id=1 >> "$(1)"
   echo vol_type=dynamic >> "$(1)"
   echo vol_name=cache >> "$(1)"
-  echo vol_size=4MiB >> "$(1)"
+  echo vol_size=89MiB >> "$(1)"
 
   echo \[persist_volume\] >> "$(1)"
   echo mode=ubi >> "$(1)"
