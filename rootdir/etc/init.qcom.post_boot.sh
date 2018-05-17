ea061a63f7c498cb620029123eb69b604bc04715
keqili <keqili@codeaurora.org>
2018-05-17 20:18:30 +0800

    msm8909w: make 400Mhz CPU freq as min frequency
    Signed-off-by: keqili <keqili@codeaurora.org>
    
    Change-Id: I7d4136833443d224a3d5ab7033eb7ef9548ee2de

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2944,7 +2944,7 @@ case "$target" in
         echo 0 > /sys/module/msm_thermal/core_control/enabled
         echo 1 > /sys/devices/system/cpu/cpu0/online
         echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        echo 400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         # enable thermal core_control now
         echo 1 > /sys/module/msm_thermal/core_control/enabled
 
