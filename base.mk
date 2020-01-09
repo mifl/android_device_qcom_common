aeefe60d0683b86da1857cb50599722d96ee570e
hoffc <hoffc@codeaurora.org>
2020-01-09 14:13:55 +0800

    Common: fix CTS testCameraFeature failure
    Match the framework feature config with camera hardware capabilities.
    
    Change-Id: Ib5af6ed6a0aa0f7b1940a118faa8695e79467905
    CRs-Fixed: 2598316

--- base.mk
+++ base.mk
@@ -974,9 +974,7 @@ PRODUCT_COPY_FILES := \
 
 ifneq ($(call is-board-platform-in-list, msm8909 msm8916 msm8952),true)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml\
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml\
-	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml
 endif
 
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
